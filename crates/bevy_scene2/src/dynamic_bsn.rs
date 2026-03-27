0ed03512f5454de333574b317fe3c90aafe975b0
Joe Buehler <jbuehler23@gmail.com>
2026-03-27 11:07:55 +0000

    Add info logging to all TypeMismatch error paths

--- crates/bevy_scene2/src/dynamic_bsn.rs
+++ crates/bevy_scene2/src/dynamic_bsn.rs
@@ -694,8 +694,7 @@ impl BsnAst {
                     }
                 }
 
-                bevy_log::info!("BSN StringLit unhandled type: {}",
-                    expected_type_registration.type_info().type_path());
+                bevy_log::info!("BSN: StringLit unhandled for type {}", expected_type_registration.type_info().type_path());
                 Err(DynamicBsnLoaderError::TypeMismatch)
             }
 
@@ -720,6 +719,7 @@ impl BsnAst {
                     reflect.apply(&bool_lit);
                     return Ok(reflect.into_partial_reflect());
                 }
+                bevy_log::info!("BSN: BoolLit type mismatch");
                 Err(DynamicBsnLoaderError::TypeMismatch)
             }
 
@@ -734,7 +734,10 @@ impl BsnAst {
                 let list_info = type_registration
                     .type_info()
                     .as_list()
-                    .map_err(|_| DynamicBsnLoaderError::TypeMismatch)?;
+                    .map_err(|_| {
+                        bevy_log::info!("BSN: List expr for non-list type {}", type_registration.type_info().type_path());
+                        DynamicBsnLoaderError::TypeMismatch
+                    })?;
                 let item_type_id = list_info.item_ty().id();
 
                 let mut dynamic_list = DynamicList::default();
@@ -837,11 +840,10 @@ fn create_reflect_default_from_type_registration(
     expected_type_registration: &TypeRegistration,
 ) -> Result<Box<dyn Reflect>, DynamicBsnLoaderError> {
     let Some(reflect_default) = expected_type_registration.data::<ReflectDefault>() else {
+        let tp = expected_type_registration.type_info().type_path().to_owned();
+        bevy_log::info!("BSN: type doesn't implement Default: {tp}");
         return Err(DynamicBsnLoaderError::TypeDoesntImplementDefault(
-            expected_type_registration
-                .type_info()
-                .type_path()
-                .to_owned(),
+            tp.clone(),
         ));
     };
     Ok(reflect_default.default())
